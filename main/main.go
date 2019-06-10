1d23cc924289931e0e832acab0e0bc6cbcbc7107
Dheeraj Pundir <44019618+dheeraj-p@users.noreply.github.com>
2019-06-11 00:23:44 +0530

    Refactor main.go
    -- Extract function for timeStamp
    -- Extract variable `snapshotsDirName`

--- main/main.go
+++ main/main.go
@@ -9,22 +9,29 @@ import (
 )
 
 func createSnapshotDir(dirname string) error {
-	if _, err := os.Stat(dirname); os.IsNotExist(err) {
-		if err = os.Mkdir(dirname, 0777); err != nil {
-			return err
-		}
+	if _, err := os.Stat(dirname); os.IsExist(err) {
+		return nil
 	}
+
+	if err := os.Mkdir(dirname, 0777); err != nil {
+		return err
+	}
+
 	return nil
 }
 
-func main() {
-	timeStamp := time.Now().Format("2006_01_02_15_04_05")
+func formattedTimeStamp() string {
+	return time.Now().Format("2006_01_02_15_04_05")
+}
 
-	if err := createSnapshotDir(".snapshots"); err != nil {
+func main() {
+	var snapshotsDirName = ".snapshots"
+
+	if err := createSnapshotDir(snapshotsDirName); err != nil {
 		fmt.Println(err)
 	}
 
-	destination := fmt.Sprintf(".snapshots/snapshot_%s.tar.gz", timeStamp)
+	destination := fmt.Sprintf("%s/snapshot_%s.tar.gz", snapshotsDirName, formattedTimeStamp())
 	file, err := os.Create(destination)
 
 	if err != nil {
@@ -32,7 +39,8 @@ func main() {
 		return
 	}
 
-	err = targzhelper.MakeTar("./", file, []string{".snapshots", ".git"})
+	var dirsToIgnore = []string{snapshotsDirName, ".git"}
+	err = targzhelper.MakeTar("./", file, dirsToIgnore)
 	if err != nil {
 		fmt.Println(err)
 	}
