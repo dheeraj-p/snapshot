9dfdd392030241e4836d95187f5e6d9c593e468a
Dheeraj Pundir <44019618+dheeraj-p@users.noreply.github.com>
2019-06-17 00:33:30 +0530

    Add test for valid args
    -- create a tmp dir to keep all the test data
    -- implement setUp and tearDown
    -- remove unnecessary directory creation
    -- coverage is now 52.4% :D

--- targzhelper/helper_test.go
+++ targzhelper/helper_test.go
@@ -27,9 +27,11 @@ func tearDownForMakeTar(dirPath string) error {
 
 func testMakeTarWithInvalidPath(t *testing.T, testDirPath string) {
 	buffer := bytes.NewBuffer([]byte{})
-	invalidDirPath := filepath.Join(testDirPath, "foo")
+	invalidDirPath := filepath.Join(testDirPath, "invalidPath")
+
 	err := MakeTar(invalidDirPath, buffer, []string{})
 	expected := fmt.Errorf("Invalid Path stat %s: no such file or directory", invalidDirPath)
+
 	if !reflect.DeepEqual(err, expected) {
 		t.Errorf("\nEXPECTED === %s\nGOT === %s\n", expected, err)
 	}
@@ -37,17 +39,29 @@ func testMakeTarWithInvalidPath(t *testing.T, testDirPath string) {
 
 func testMakeTarWithFile(t *testing.T, testDirPath string) {
 	buffer := bytes.NewBuffer([]byte{})
-	testFilePath := filepath.Join(testDirPath, "bar")
+	testFilePath := filepath.Join(testDirPath, "file")
 	os.Create(testFilePath)
+
 	err := MakeTar(testFilePath, buffer, []string{})
 	expected := fmt.Errorf("%s is not a directory", testFilePath)
+
 	if !reflect.DeepEqual(err, expected) {
 		t.Errorf("\nEXPECTED === %s\nGOT === %s\n", expected, err)
 	}
 }
 
+func testMakeTarWithValidArgs(t *testing.T, testDirPath string) {
+	buffer := bytes.NewBuffer([]byte{})
+	testFilePath := filepath.Join(testDirPath, "validDir")
+	os.Mkdir(testFilePath, 0777)
+
+	err := MakeTar(testFilePath, buffer, []string{})
+	if err != nil {
+		t.Errorf("Unexpected error occured %s", err.Error())
+	}
+}
+
 func TestMakeTar(t *testing.T) {
-	os.Mkdir("tmp", 0777)
 	snapshotDir, err := setUpForMakeTar()
 
 	if err != nil {
@@ -57,6 +71,7 @@ func TestMakeTar(t *testing.T) {
 
 	testMakeTarWithInvalidPath(t, snapshotDir)
 	testMakeTarWithFile(t, snapshotDir)
+	testMakeTarWithValidArgs(t, snapshotDir)
 
 	if err := tearDownForMakeTar(snapshotDir); err != nil {
 		t.Errorf("Could not tearDown tests")
